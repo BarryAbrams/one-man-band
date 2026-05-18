13049c63547eec0e84f59ab9db9f030ea617c4e7
Your Name <you@example.com>
2026-05-18 14:27:47 -0500

    merge

---
+++ scripts/rp2040_minimal_link.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import argparse
+import signal
+import struct
+import sys
+import time
+from dataclasses import dataclass
+
+from smbus2 import SMBus
+
+
+def main() -> int:
+   print("python is running")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
