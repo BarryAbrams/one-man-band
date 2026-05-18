ba86dd39e9fb01bb2e34a056056427eb2dd9b44f
Your Name <you@example.com>
2026-05-18 14:31:54 -0500

    merge

--- scripts/rp2040_minimal_link.py
+++ scripts/rp2040_minimal_link.py
@@ -1,16 +1,29 @@
+from smbus import SMBus
+import curses
 
-from smbus2 import SMBus
-import sys
+addr = 0x12 # bus address
+bus = SMBus(1) # indicates /dev/ic2-1
 
-addr = 0x12
-bus = SMBus(1)
+numb = 1
 
+give_move = curses.initscr()
+curses.noecho()
+give_move.nodelay(1) # set getch() non-blocking
 
-def main() -> int:
-   print("python is running")
-   while True:
-      bus.write_byte(addr, 0x41)
-   return 0
+#print ("Enter 1 for ON or 0 for OFF")
+num = 0
+while 1:
 
-if __name__ == "__main__":
-    main()
+	push_key = give_move.getch()#input(">>>>   ")
+
+	if push_key == ord("1"):
+		if(num <= 250):
+			num += 5
+			#bus.write_byte(addr, 0x1) # switch it on
+			bus.write_byte(addr, num)
+	
+	elif push_key == ord("0"):
+		if(num >= 5):
+			num -= 5
+			#bus.write_byte(addr, 0x0) # switch it on
+			bus.write_byte(addr, num)
